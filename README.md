27b7a556af0f07a2f9daba5eee054d8567f1952c
Reinaldo Pacheco <reinaldo.pacheco@usach.cl>
2023-08-12 23:37:07 -0400

    se agregan archivos y actualizo el readme
    se agregaron los archivos .html, .css y .js además de actualizar el readme con funcionalidades que quiero agregar en un futuro

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # calculadora
+# esta es una calculadora básica creada con html, css y javascript, es la primera versión, cuando tenga tiempo iré agregando más funcionalidades y mejorando el diseño.
+# proximas funcionalidades que quiero implementar:
+#  agregar un historial de operaciones realizadas
+#  agregar más operaciones como coseno, seno, porcentaje
+#  poner un boton para cambiar entre modo oscuro y claro
+#  agregar mas transiciones
---
+++ main.js
@@ -0,0 +1,71 @@
+const pantalla = document.querySelector(".pantalla");
+const botones = document.querySelectorAll(".boton");
+
+let mostrarError = false;
+
+botones.forEach(boton => {
+    boton.addEventListener("click", () => {
+        if (boton.id === "c") {
+            pantalla.textContent = "0";
+            mostrarError = false;
+            return;
+        }
+
+        if (mostrarError) {
+            return;
+        }
+
+        const botonPresionado = boton.textContent;
+
+        if (botonPresionado === "←") {
+            if (pantalla.textContent.length === 1 || pantalla.textContent === "Error") {
+                pantalla.textContent = "0";
+            } else {
+                pantalla.textContent = pantalla.textContent.slice(0, -1);
+            }
+            return;
+        }
+
+        if (botonPresionado === "=") {
+            try {
+                pantalla.textContent = eval(pantalla.textContent);
+            } catch {
+                pantalla.textContent = "Error!";
+                mostrarError = true;
+            }
+            return;
+        }
+
+        if (pantalla.textContent === "0" || pantalla.textContent === "Error") {
+            pantalla.textContent = botonPresionado;
+        } else {
+            pantalla.textContent += botonPresionado;
+        }
+    });
+});
+
+document.addEventListener("keydown", event => {
+    const key = event.key;
+    if (/[\d./*\-+]/.test(key)) {
+        event.preventDefault();
+        if (mostrarError) {
+            return;
+        }
+        if (key === "*") {
+            pantalla.textContent += key;
+        } else if (key === "Enter") {
+            try {
+                pantalla.textContent = eval(pantalla.textContent);
+            } catch {
+                pantalla.textContent = "Error!";
+                mostrarError = true;
+            }
+        } else {
+            if (pantalla.textContent === "0" || pantalla.textContent === "Error") {
+                pantalla.textContent = key;
+            } else {
+                pantalla.textContent += key;
+            }
+        }
+    }
+});
